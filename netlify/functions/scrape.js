06fb50c71662d6da222d729e599b67dadd8c4cb1
tmavoslav <158513971+PeterTomascik@users.noreply.github.com>
2025-08-04 21:41:40 +0200

    Create scrape.js

---
+++ netlify/functions/scrape.js
@@ -0,0 +1,17 @@
+{
+  "name": "sktorrent-scraper-netlify",
+  "version": "1.0.0",
+  "description": "Netlify Function for scraping online.sktorrent.eu",
+  "main": "netlify/functions/scrape.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "axios": "^1.7.2",
+    "cheerio": "^1.0.0-rc.12",
+    "entities": "^4.5.0"
+  }
+}
