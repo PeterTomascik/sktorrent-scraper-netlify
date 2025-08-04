3f4ee05c85b19e646072cb901a29ad9944f78426
tmavoslav <158513971+PeterTomascik@users.noreply.github.com>
2025-08-04 23:24:10 +0200

    Update scrape.js

--- netlify/functions/scrape.js
+++ netlify/functions/scrape.js
@@ -5,14 +5,18 @@ const { decode } = require("entities");
 
 // Hlavičky pre priame požiadavky na online.sktorrent.eu
 const commonHeaders = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/114.0.0.0 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'Accept-Encoding': 'identity', // <--- ZMENA JE TU!
+    'Accept-Encoding': 'identity',
     'Accept-Language': 'en-US,en;q=0.9,sk;q=0.8,cs;q=0.7',
     'Connection': 'keep-alive',
     'Upgrade-Insecure-Requests': '1',
     'Referer': 'https://online.sktorrent.eu/',
-    'DNT': '1'
+    'DNT': '1',
+    'Sec-Fetch-Dest': 'document', // Nové
+    'Sec-Fetch-Mode': 'navigate', // Nové
+    'Sec-Fetch-Site': 'same-origin', // Nové
+    'Sec-Fetch-User': '?1' // Nové
 };
 
 function removeDiacritics(str) {
